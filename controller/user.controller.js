48500413a2ff92008efbe9f0e1dc56b1a64da1ce
morshedAhmed <morshed50r@gmail.com>
2022-09-03 06:01:53 -0700

    last commot

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,53 +1,60 @@
-const users = [
-  {
-    id: 1,
-    name: "morshed Ahmed",
-    gender: "male",
-    contact: "01900000",
-    address: "bogura",
-    photoUrl:
-      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
-  },
-  {
-    id: 2,
-    name: "morshed Ahmed",
-    gender: "male",
-    contact: "01900000",
-    address: "bogura",
-    photoUrl:
-      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
-  },
-  {
-    id: 3,
-    name: "morshed Ahmed",
-    gender: "male",
-    contact: "01900000",
-    address: "bogura",
-    photoUrl:
-      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
-  },
-  {
-    id: 4,
-    name: "morshed Ahmed",
-    gender: "male",
-    contact: "01900000",
-    address: "bogura",
-    photoUrl:
-      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
-  },
-  {
-    id: 5,
-    name: "morshed Ahmed",
-    gender: "male",
-    contact: "01900000",
-    address: "bogura",
-    photoUrl:
-      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
-  },
-];
+// const users = [
+//   {
+//     id: 1,
+//     name: "morshed Ahmed",
+//     gender: "male",
+//     contact: "01900000",
+//     address: "bogura",
+//     photoUrl:
+//       "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+//   },
+//   {
+//     id: 2,
+//     name: "morshed Ahmed",
+//     gender: "male",
+//     contact: "01900000",
+//     address: "bogura",
+//     photoUrl:
+//       "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+//   },
+//   {
+//     id: 3,
+//     name: "morshed Ahmed",
+//     gender: "male",
+//     contact: "01900000",
+//     address: "bogura",
+//     photoUrl:
+//       "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+//   },
+//   {
+//     id: 4,
+//     name: "morshed Ahmed",
+//     gender: "male",
+//     contact: "01900000",
+//     address: "bogura",
+//     photoUrl:
+//       "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+//   },
+//   {
+//     id: 5,
+//     name: "morshed Ahmed",
+//     gender: "male",
+//     contact: "01900000",
+//     address: "bogura",
+//     photoUrl:
+//       "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+//   },
+// ];
+
+const users = require("../coustomData/users.data");
+const fs = require("fs");
+
+// const users = fs.readFile("../coustomData/user.data.json");
 
 module.exports.randomUser = (req, res) => {
-  res.send(users);
+  const random = Math.floor(Math.random() * users.length) + 1;
+  const filter = users.find((user) => user.id === random);
+  res.send(filter);
 };
 
 module.exports.allUser = (req, res) => {
---
+++ coustomData/users.data.js
@@ -0,0 +1,49 @@
+const users = [
+  {
+    id: 1,
+    name: "morshed Ahmed",
+    gender: "male",
+    contact: "01900000",
+    address: "bogura",
+    photoUrl:
+      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+  },
+  {
+    id: 2,
+    name: "morshed Ahmed",
+    gender: "male",
+    contact: "01900000",
+    address: "bogura",
+    photoUrl:
+      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+  },
+  {
+    id: 3,
+    name: "morshed Ahmed",
+    gender: "male",
+    contact: "01900000",
+    address: "bogura",
+    photoUrl:
+      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+  },
+  {
+    id: 4,
+    name: "morshed Ahmed",
+    gender: "male",
+    contact: "01900000",
+    address: "bogura",
+    photoUrl:
+      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+  },
+  {
+    id: 5,
+    name: "morshed Ahmed",
+    gender: "male",
+    contact: "01900000",
+    address: "bogura",
+    photoUrl:
+      "https://images.unsplash.com/photo-1662044286007-10905b5456e7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=775&q=80",
+  },
+];
+
+module.exports = users;
